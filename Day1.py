47ca704722f99370137290a5fb22e690b6cc9279
MhussainD4772 <mohammedjoe53@gmail.com>
2025-06-23 22:00:43 -0400

    Modified Day1

--- Day1.py
+++ Day1.py
@@ -1,53 +1,53 @@
 #✅ Task #1 — Ask the user for their name and print:
 
-# name = input("What is your name ?")
+name = input("What is your name ?")
 
-# print(f"Hello {name}!!")
+print(f"Hello {name}!!")
 
 # ==================================================================>>>
 
 #✅ Task #2 — Ask the user to enter two numbers, then print their sum.
 
-# first_number = int(input("What is you first numner ?"))
-# second_number = int(input("What is your second number ?"))
+first_number = int(input("What is you first numner ?"))
+second_number = int(input("What is your second number ?"))
 
-# print(first_number + second_number)
+print(first_number + second_number)
 
 # ==================================================================>>>
 
 #🧠 Bonus Goal: Ask the user for three numbers, then add them using the sum() function and print the result.
 
-# first_number = int(input("First number ?"))
-# second_number = int(input("Second number ?"))
-# third_number = int(input("Third number ?"))
+first_number = int(input("First number ?"))
+second_number = int(input("Second number ?"))
+third_number = int(input("Third number ?"))
 
-# print(sum([first_number, second_number, third_number]))
+print(sum([first_number, second_number, third_number]))
 
 # ==================================================================>>>
 
 # ✅ Task #3 — Your Next Goal: Ask the user for a number.
 #  If the number is greater than 10, print "Big number!" If it’s 10 or less, print "Small number."
 
-# number = int(input("Tell me the number that you wanna choose ?"))
+number = int(input("Tell me the number that you wanna choose ?"))
 
-# if number > 10:
-#     print("Big number!")
+if number > 10:
+    print("Big number!")
 
-# else:
-#     print("Small number!")
+else:
+    print("Small number!")
 
 
 # ==================================================================>>>
 
 # ✅ Task #4 — Even or Odd Checker, Goal: Ask the user for a number. Print whether the number is even or odd.
 
-# number = int(input("Enter a number and python will tell you if it is odd or even !"))
+number = int(input("Enter a number and python will tell you if it is odd or even !"))
 
-# if number % 2 == 0:
-#     print("Even number")
+if number % 2 == 0:
+    print("Even number")
    
-# else:
-#     print("Odd number")
+else:
+    print("Odd number")
 
 # ==================================================================>>>
 
@@ -62,6 +62,10 @@ if number % 2 == 0:
 else:
     print("Odd number")
     print("Odd numbers love being unique!")
+
+# ==================================================================>>>
+
+
 
 
 
